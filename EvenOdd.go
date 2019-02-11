6709fd2a7b2fde076680f1c62bd69e293a63f753
priyadharsshini31 <33366245+priyadharsshini31@users.noreply.github.com>
2019-02-11 17:39:32 +0530

    Example program for odd/even number

---
+++ EvenOdd.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main() {
+input := 5
+if input%2 == 0 {
+  fmt.Println("the number is an even number")
+} else {
+     fmt.Println("The number is an odd number")
+}
