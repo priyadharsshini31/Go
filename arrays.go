6160cd2947ed4d60af555eb8bb841e9425bc3ffd
priyadharsshini31 <33366245+priyadharsshini31@users.noreply.github.com>
2019-06-05 13:14:35 +0530

    Basics of array

---
+++ arrays.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// need to specify the length for arrays
+
+func main() {
+	x  := [5]int{1,2,3,4,5}
+	fmt.Println (x)
+	
+	var y [5]int
+	fmt.Println(y) 
+	
+}
