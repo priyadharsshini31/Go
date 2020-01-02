a11e4599a0363aba4315702a75443453d60fef95
priyadharsshini31 <33366245+priyadharsshini31@users.noreply.github.com>
2020-01-02 14:57:19 +0530

    Update removeoccurance1.go

--- Programs/removeoccurance1.go
+++ Programs/removeoccurance1.go
@@ -8,7 +8,8 @@ import (
 func main() {
 	
 	input := "priyadharsshini"
-	output := strings.Replace(input,"a","",-1)
+	output := strings.Replace(input,"a","",-1) //If n < 0, there is no limit on the number of replacements or n number if replacements
+
 	fmt.Println("the final output is", output)
 	
 }
